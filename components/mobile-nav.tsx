6b471699d54d11759aa1f400613b05941290c6ec
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-23 08:24:43 -0400

    fix: remove drawer trigger as drawer is always open on mobile

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -6,7 +6,6 @@ import {
 	Drawer,
 	DrawerContent,
 	DrawerTitle,
-	DrawerTrigger,
 } from "@/components/ui/drawer";
 import { useIsMobile } from "@/hooks/use-mobile";
 
@@ -16,29 +15,6 @@ export function MobileNav() {
 	return (
 		<Drawer modal={false} open={isMobile}>
 			<DrawerTitle className="hidden">Navigation</DrawerTitle>
-			<DrawerTrigger asChild>
-				<Button
-					variant="ghost"
-					className="size-8 gap-4 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="currentColor"
-						className="!size-6"
-					>
-						<title>Toggle Menu</title>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M3.75 9h16.5m-16.5 6.75h16.5"
-						/>
-					</svg>
-					<span className="sr-only">Toggle Menu</span>
-				</Button>
-			</DrawerTrigger>
 			<DrawerContent className="max-h-[80svh] p-0">
 				<div className="overflow-auto p-6">
 					<div className="flex flex-col space-y-3">
