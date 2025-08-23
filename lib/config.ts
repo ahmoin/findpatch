1d87bd2629d1c9912820a8095f779bb4002782e8
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-08-23 08:11:39 -0400

    refactor: make mobile nav open by default

--- lib/config.ts
+++ lib/config.ts
@@ -2,9 +2,8 @@ export const siteConfig = {
 	name: "Findpatch",
 	url: "https://findpatch.vercel.app",
 	ogImage: "https://findpatch.vercel.app/og.jpg", // TODO: add og image
-	description:
-	  " ", // TODO: add description and use siteConfig in layout.tsx
-  }
+	description: " ", // TODO: add description and use siteConfig in layout.tsx
+};
 
 export const META_THEME_COLORS = {
 	light: "#ffffff",
